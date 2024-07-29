a80bc81954e0b208b6c7e41f698a3f18bdde048d
ABid hasan <abeydhasan134@gmail.com>
2024-07-29 16:09:02 +0600

    cookie perser seting for jwt

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ require('dotenv').config()
 const port= process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 // middleware
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   origin: ["http://localhost:5173"], 
   credentials: true, 
 }))
+
+    // middleware to verify user
+    const verifyToken=(req,res,next)=>{
+
+    }
+
+    const cookieOptions={
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production"?true:false,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    }
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.il352b3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -36,10 +50,12 @@ const client = new MongoClient(uri, {
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
       app.post('/jwt',async(req,res)=>{
         const user=req.body;
-        console.log("user get from here",user)
+        // console.log("user get from here",user)
         const token=jwt.sign(user,process.env.ACCESS_TOKEN,{ expiresIn: '1h' })
-        res.send(token);
+        // res.cookie("token", token,cookieOptions).send({ success: 'true'});
+        res.cookie('token',token,cookieOptions).send({ success: 'true'});
       })
+  
 
     } finally {
       // Ensures that the client will close when you finish/error
